d74986a6bc4d95ada369525b564cbf46a4142912
Victor Rodrigues <vitumail@gmail.com>
2021-03-23 16:46:01 +0100

    Add license

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule ElixirDoctor.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: [{:erlang_doctor, "~> 0.1"}]
+      deps: [{:erlang_doctor, "~> 0.1"}],
+      package: [
+        licenses: ["Apache-2.0"]
+      ]
     ]
   end
 end
