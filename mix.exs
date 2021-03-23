e1653b1d90d32bd57d301aee9f8c7d5a4ad692d4
Victor Rodrigues <vitumail@gmail.com>
2021-03-23 16:49:42 +0100

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -8,7 +8,10 @@ defmodule ElixirDoctor.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: [{:erlang_doctor, "~> 0.1"}],
+      deps: [
+        {:erlang_doctor, "~> 0.1"},
+        {:ex_doc, "~> 0.24.1", only: :docs}
+      ],
       package: [
         licenses: ["Apache-2.0"],
         links: %{"GitHub" => "https://github.com/rodrigues/elixir_doctor"}
