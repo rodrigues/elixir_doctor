6c9bb8527b757ca77664ee0cd208502a7006010d
Victor Rodrigues <vitumail@gmail.com>
2021-03-23 16:47:21 +0100

    Fix project config

--- mix.exs
+++ mix.exs
@@ -4,12 +4,14 @@ defmodule ElixirDoctor.MixProject do
   def project do
     [
       app: :elixir_doctor,
+      description: "Tiny wrapper on top of erlang_doctor",
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: [{:erlang_doctor, "~> 0.1"}],
       package: [
-        licenses: ["Apache-2.0"]
+        licenses: ["Apache-2.0"],
+        links: %{"GitHub" => "https://github.com/rodrigues/elixir_doctor"}
       ]
     ]
   end
